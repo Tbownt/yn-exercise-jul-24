758ac1a8f95b36993df45a4c9f6505f142f2d330
Tbownt <98657140+Tbownt@users.noreply.github.com>
2024-07-11 00:44:08 -0400

    refactor: Task 2 completed

--- src/views/Form.view.tsx
+++ src/views/Form.view.tsx
@@ -5,11 +5,15 @@ import { Controller, useForm } from 'react-hook-form'
 
 import { useUpdateAnswers } from '../api-hooks/useUpdateAnswers'
 import { CheckboxGroup } from '../components'
+import { CustomCheckboxProps } from '../components/CheckboxGroup'
+import { DomainOption } from '../domain/types'
 import { useAnswersStore } from '../state'
 
 import { validationSchema } from './Form.config'
 
-type Interest = { [key: string]: { label: string } }
+type InterestType = { [key: string]: { label: string } }
+
+type SubmitInterestsData = { id: string; label: string; checked: boolean }
 
 export const FormView = () => {
     const answers = useAnswersStore(state => state.getAnswers())
@@ -26,16 +30,29 @@ export const FormView = () => {
     const updateAnswersMutation = useUpdateAnswers()
 
     const onSubmit = handleSubmit(formData => {
-        console.log(formData)
+        //The data passed to answers.interest must be the same as that passed to
+        //as SubmitInterestData in order to satisfy the
+        //endpoint that we would use for the rest of the answers.
+        const interests = formData.interests as SubmitInterestsData[]
+
+        //this way we can process our data submitted to our endpoint without any
+        //and without changing the original answers.interests
+        const newInterests: DomainOption[] = interests.map(interest => ({
+            [interest.id]: {
+                isChecked: interest.checked,
+                label: interest.label,
+            },
+        }))
+
         updateAnswersMutation.mutate({
             name: formData.name,
             mail: formData.mail,
             age: formData.age,
-            interests: [],
+            interests: newInterests,
         })
     })
 
-    const mappedInterests = answers.interests.map((interest: Interest) => {
+    const mappedInterests = answers.interests.map((interest: InterestType) => {
         //Grabs the id from the original answers.interest
         //the value of id is for example['31235']
         const id = Object.keys(interest)[0]
@@ -44,8 +61,30 @@ export const FormView = () => {
         return {
             id,
             label: interest[id].label,
+            checked: false,
         }
     })
+
+    const internalOnChange = (
+        interestsProps: CustomCheckboxProps[],
+        onChange: (event: unknown) => void,
+    ) => {
+        //Get the array of options we currently have
+        const updatedInterests = interestsProps.map(option => {
+            //Find the ID that was clicked and see if it matches any of our
+            //with any of our interestIs
+            const interestID = mappedInterests.find(
+                interest => interest.id === option.id,
+            )
+            if (interestID) {
+                //If we found a match, we mark it true, otherwise
+                //we mark it false.
+                interestID.checked = option.checked ?? false
+            }
+            return option
+        })
+        onChange(updatedInterests)
+    }
 
     return (
         <div id="form-view">
@@ -116,7 +155,9 @@ export const FormView = () => {
                             id="interests-checkbox-group"
                             label="Interest"
                             options={mappedInterests}
-                            onChange={() => onChange(mappedInterests)}
+                            onChange={interests =>
+                                internalOnChange(interests, onChange)
+                            }
                             helperText={errors.interests?.message || ''}
                             error={Boolean(errors.interests)}
                         />
